993c771b0a4b8f9ee5a1c84f64cb855bb581d9fa
Vinicius Lopes <viniciuslopeslps@gmail.com>
2017-07-14 22:40:00 -0300

    begin

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 var express = require("express");
 var app = express();
-
 var PORT = process.env.PORT || 3000;
+
+var todos = [{
+	"id": 1,	
+	"desciption": "meet mom for lunch",
+	"completed": false
+},{
+	"id": 2,	
+	"desciption": "go to market",
+	"completed": false
+}];
+
 
 app.get("/", function(req, res){
 	res.send("TODO API Root");
+});
+
+app.get("/todos", function(req, res){
+	res.json(todos);
 });
 
 app.listen(PORT, function(){
