a5e2d1ed145c4a425fa6b069a75f06ef2fbb76f2
Vinicius Lopes <viniciuslopeslps@gmail.com>
2017-07-17 13:30:36 -0300

    cadastrando todos

--- app.js
+++ app.js
@@ -1,17 +1,12 @@
 var express = require("express");
+var bodyParser = require("body-parser");
 var app = express();
 var PORT = process.env.PORT || 3000;
+var todos = [];
+var todoNextId = 1;
 
-var todos = [{
-	"id": 1,	
-	"desciption": "meet mom for lunch",
-	"completed": false
-},{
-	"id": 2,	
-	"desciption": "go to market",
-	"completed": false
-}];
-
+//body parser é um middleware a nivel de aplicacao, ou seja, vai interceptar todas as requisiçoes e pegar os seus jsons
+app.use(bodyParser.json());
 
 app.get("/", function(req, res){
 	res.send("TODO API Root");
@@ -38,6 +33,14 @@ app.get("/todos/:id", function(req, res){
 	}
 	console.log("Not found");
 	res.status(404).send();
+});
+
+app.post("/todos", function(req, res){
+    var body = req.body;
+    body.id = todoNextId++;
+    todos.push(body);
+    console.log("Adicionado com sucesso!");
+    res.json(body);
 });
 
 app.listen(PORT, function(){
