ed06f565beb5f156b4818bda26d57c1d7e16ab64
Vinicius Lopes <viniciuslopeslps@gmail.com>
2017-07-16 21:02:11 -0300

    todo by item

--- app.js
+++ app.js
@@ -21,6 +21,25 @@ app.get("/todos", function(req, res){
 	res.json(todos);
 });
 
+//colocar :nomeDoParametro quando quiser colocar um parametro adicional	
+app.get("/todos/:id", function(req, res){
+	var id = parseInt(req.params.id);
+	var matched;
+	for(var i=0; i < todos.length;i++){
+		if(todos[i].id == id){
+			matched = todos[i];
+			break;
+		}
+	}
+
+	if(matched !== undefined){
+		console.log("found " + matched);
+		res.json(matched);
+	}
+	console.log("Not found");
+	res.status(404).send();
+});
+
 app.listen(PORT, function(){
 	console.log("Server running at: " + PORT);
 });
