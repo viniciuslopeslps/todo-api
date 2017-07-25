613736e9bc7400082f1423e167c40227acd91760
Vinicius Lopes <viniciuslopeslps@gmail.com>
2017-07-25 13:45:42 -0300

    login

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var _ = require("underscore");
 var db = require("./db.js");
+var bcrypt = require("bcrypt");
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -189,6 +190,27 @@ app.post("/users", function(req, res){
 	}, function (e) {
 		res.status(400).json(e);
 	});                  
+});
+
+app.post("/users/login", function(req, res){
+    var body = _.pick(req.body, "email", "password"); //Return a copy, filtered to only values in whitelisted keys 
+    if(typeof body.email !== "string" || typeof body.password !== "string"){
+       return res.status(400).send();
+    }
+    db.user.findOne({
+        where: {
+            email: body.email
+        }
+    }).then(function(user){
+        if(!user || !bcrypt.compareSync(body.password, user.get("password_hash"))){
+            return res.status(401).send();   
+        }
+        
+        res.json(user.toPublicJson());
+    }, function(e){
+        res.status(500).json({"error":e});
+    })
+    
 });
 
 db.sequelize.sync().then(function() {
