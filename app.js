2565a5a9ad2dd871c4dd2ec184ee243767cd4011
Vinicius Lopes <viniciuslopeslps@gmail.com>
2017-07-25 20:33:42 -0300

    refactor

--- app.js
+++ app.js
@@ -194,23 +194,12 @@ app.post("/users", function(req, res){
 
 app.post("/users/login", function(req, res){
     var body = _.pick(req.body, "email", "password"); //Return a copy, filtered to only values in whitelisted keys 
-    if(typeof body.email !== "string" || typeof body.password !== "string"){
-       return res.status(400).send();
-    }
-    db.user.findOne({
-        where: {
-            email: body.email
-        }
-    }).then(function(user){
-        if(!user || !bcrypt.compareSync(body.password, user.get("password_hash"))){
-            return res.status(401).send();   
-        }
-        
-        res.json(user.toPublicJson());
-    }, function(e){
-        res.status(500).json({"error":e});
-    })
     
+    db.user.authenticate(body).then(function(user){
+    	res.json(user.toPublicJson());
+    },function(e){
+    	res.status(401).send();
+    });
 });
 
 db.sequelize.sync().then(function() {
--- models/user.js
+++ models/user.js
@@ -3,51 +3,73 @@ var _ = require("underscore");
 
 
 module.exports = function(sequelize, DataTypes){
-    return sequelize.define('user', {
-        "email": {
-            "type": DataTypes.STRING,
-            "allowNull": false,
-            "unique": true,
-            "validate": {
-                "isEmail": true
-            }
-        },
-        "salt":{
-        	"type": DataTypes.STRING
-        },
-        "password_hash": {
-        	"type": DataTypes.STRING
-        },
-        "password": {
-            "type": DataTypes.VIRTUAL,
-            "allowNull": false,
-            "validate":{
-                len: [7, 100]
-            },
-            set: function(value){ //criando um "set" para a senha
-            	var salt = bcrypt.genSaltSync(10); //tipo de código para se único
-            	var hashedPassword = bcrypt.hashSync(value, salt);
-            	//armazena virtualmente
-            	this.setDataValue("password", value);
-            	this.setDataValue("salt", salt);
-            	this.setDataValue("password_hash", hashedPassword);
-            }
-        }
-    },{
-        "hooks":{
-            //é possível executar funcoes antes e depois de inserir um dado
-            //estamos colocando um email para lowercase
-            "beforeValidate": function(user, options){
-                if(typeof user.email === 'string'){
-                    user.email = user.email.toLowerCase();
-                }
-            }
-        },
-        "instanceMethods": {
-        	"toPublicJson": function(){
-        		var json = this.toJSON();
-        		return _.pick(json, "id", "email", "updatedAt", "createdAt");
-        	}
-        }
-    });
+	var user = sequelize.define('user', {
+		"email": {
+			"type": DataTypes.STRING,
+			"allowNull": false,
+			"unique": true,
+			"validate": {
+				"isEmail": true
+			}
+		},
+		"salt":{
+			"type": DataTypes.STRING
+		},
+		"password_hash": {
+			"type": DataTypes.STRING
+		},
+		"password": {
+			"type": DataTypes.VIRTUAL,
+			"allowNull": false,
+			"validate":{
+				len: [7, 100]
+			},
+			set: function(value){ //criando um "set" para a senha
+				var salt = bcrypt.genSaltSync(10); //tipo de código para se único
+				var hashedPassword = bcrypt.hashSync(value, salt);
+				//armazena virtualmente
+				this.setDataValue("password", value);
+				this.setDataValue("salt", salt);
+				this.setDataValue("password_hash", hashedPassword);
+			}
+		}
+	},{
+		"hooks":{
+			//é possível executar funcoes antes e depois de inserir um dado
+			//estamos colocando um email para lowercase
+			"beforeValidate": function(user, options){
+				if(typeof user.email === 'string'){
+					user.email = user.email.toLowerCase();
+				}
+			}
+		},
+		"classMethods":{
+			authenticate: function(body){
+				return new Promise(function(resolve, reject){
+					if(typeof body.email !== "string" || typeof body.password !== "string"){
+						return reject();
+					}
+					user.findOne({
+						where: {
+							email: body.email
+						}
+					}).then(function(user){
+						if(!user || !bcrypt.compareSync(body.password, user.get("password_hash"))){
+							return reject();   
+						}
+						resolve(user);
+					}, function(e){
+						reject();
+					})
+				});
+			}
+		},
+		"instanceMethods": {
+			"toPublicJson": function(){
+				var json = this.toJSON();
+				return _.pick(json, "id", "email", "updatedAt", "createdAt");
+			}
+		}
+	});
+	return user;
 }
